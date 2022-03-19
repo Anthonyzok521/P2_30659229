6c3afce0bfeec8ce6578a035fe81045f2d29289e
AC GAMES <66536793+Anthonyzok521@users.noreply.github.com>
2022-03-19 17:34:05 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const path = require("path");
 
 app.get('/', (req, res)=>{
 
-	res.sendFile(__dirname + "/index.html");
+	res.sendFile(path.join(__dirname + "/index.html"));
 });
 app.listen(process.env.PORT || 5000);
